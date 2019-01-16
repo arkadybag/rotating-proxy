f2139ddf307d4b5a122a4868d94a636496ea2376
arkady <arkasha90@gmail.com>
2019-01-16 10:11:51 +0300

    split project

--- main.go
+++ main.go
@@ -3,27 +3,28 @@ package main
 import (
 	"bufio"
 	"github.com/google/tcpproxy"
+	"github.com/storyicon/golang-proxy/dao"
+	"github.com/storyicon/golang-proxy/model"
 	"log"
-	"myProxy/rotateProxy"
 	"net"
 )
 
 func main() {
-	proxyServer := rotateProxy.NewProxyServer()
-	proxyServer.Init()
-
 	localAddr := ":8080"
 	ln, err := net.Listen("tcp", localAddr)
 	if err != nil {
 		log.Println("local address can not connect:", localAddr, err.Error())
 	}
 
+	ips := make(chan string, 50)
+	go getProxyUrl(ips)
+
 	for {
 		c, err := ln.Accept()
 		if err != nil {
 			log.Println("local can not accept connect", err.Error())
 		}
-		go serveConn(c, proxyServer.GetProxy())
+		go serveConn(c, <-ips)
 	}
 }
 
@@ -41,4 +42,21 @@ func serveConn(c net.Conn, proxyIpPort string) {
 	log.Println("handle for:", c.LocalAddr(), c.RemoteAddr())
 	target.HandleConn(c)
 	c.Close()
+}
+
+func getProxyUrl(ips chan string) {
+	for {
+		query := "select * from proxy order by score desc limit 50"
+
+		proxy, err := dao.GetSQLResult("proxy", query)
+		if err != nil {
+			getProxyUrl(ips)
+		}
+
+		proxyModels := *proxy.(*[]model.Proxy)
+
+		for _, proxyModel := range proxyModels {
+			ips <- proxyModel.Content
+		}
+	}
 }
