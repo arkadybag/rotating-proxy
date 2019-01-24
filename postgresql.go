335dfc29ef931341c0e28aef9b9133faa99fb18a
arkady <arkasha90@gmail.com>
2019-01-24 21:07:44 +0300

    add env variables

--- postgresql.go
+++ postgresql.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
+)
+
+var (
+	host     = os.Getenv("host")
+	port     = os.Getenv("port")
+	user     = os.Getenv("user")
+	dbname   = os.Getenv("dbname")
+	password = os.Getenv("password")
 )
 
 func NewPostgreSQL() (db *gorm.DB, err error) {
-	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
-		"89.179.126.31", "5432",
-		"proxy", "proxy", "ghbdtn",
-	)
+	dbString := "host=%s port=%s user=%s dbname=%s password=%s"
+	pgConf := fmt.Sprintf(dbString, host, port, user, dbname, password)
+
+	connStr := fmt.Sprintf(pgConf)
 
 	db, err = gorm.Open("postgres", connStr)
 	if err != nil {
